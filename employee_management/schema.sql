7a0b7f686ec1e667677f9f0f03d38a4cd3953dc8
Likhitha033 <likhithasunkavalli@gmail.com>
2026-01-07 16:26:58 -0700

    Add tables for Employee Management System
    Added departments and employees tables with foreign key constraint.

--- employee_management/schema.sql
+++ employee_management/schema.sql
@@ -1,1 +1,16 @@
+----departments table
+create table departments(
+  dept_Id int primary key,
+  dept_name varchar(50),
+  );
 
+-----Employees table
+create table employees (
+emp_Id int,
+emp_name varchar(100),
+salary decimal(10,2),
+hire_date DATE,
+dept_Id int
+foreign key (dept_Id) references departments (Dept_Id)
+  );
+  
